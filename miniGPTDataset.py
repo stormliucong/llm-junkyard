a35bcccf9b40001b0e663bfb811d429658e0dff2
Cong Liu <stormliucong@gmail.com>
2025-07-16 12:02:09 -0400

    fix tokenizer & using 1 token sliding window

--- miniGPTDataset.py
+++ miniGPTDataset.py
@@ -5,31 +5,35 @@ import tiktoken
 
 TRAINING_DATASET = "trainset.txt"
 VAL_DATASET = "val.txt"
-BATCH_SIZE = batch_size
+BATCH_SIZE = 32
 
 class GPTDataset(Dataset):
   def __init__(self, file_path, max_seq=512):
+    self.max_seq = max_seq
     with open(file_path,"r") as f:
       content = f.read()
 
     # tokenize and encoder
-    sequences = titoken.encoder(content, encoder = "GPT2")
-
-    self.num_seq = len(sequences) // max_seq + 1
+    self.tokenizer = tiktoken.get_encoding("GPT2")
+    self.sequences = self.tokenizer.encode(content)
+      
+    # make sure the boundary is not exceeded
+    self.num_seq = len(self.sequences) - self.max_seq - 1
+    assert self.num_seq > 0, "Dataset too short for given max_seq"
 
   def __len__(self):
-    return len(self.num_seq)
+    return self.num_seq
 
-  def __getitems__(self, idx):
-    # each instance
-    seq_idx = idx * max_seq
-    input_start = seq_idx
-    input_end = min(len(sequences), input_end + max_seq)
-    target_start = seq_idx + 1
-    target_end = min(len(sequences), target_start + max_seq)
-    input_ids = sequences[seq_idx : seq_idx + max_seq]
-    target = sequence[seq_idx+1 : seq_idx+ 1 + max_seq]
-
+  def __getitem__(self, idx):
+    # each instance with overlap sliding window 1 token.
+    input_ids = self.sequences[idx : self.max_seq]
+    target = self.sequences[idx + 1 : self.max_seq + 1]
+    
+    # add padding only occur when num_seq = 0.
+    if len(input_ids) < self.max_seq:
+      input_ids.extend([0] * (self.max_seq - len(input_ids)))
+      target.extend([0] * ( self.max_seq - len(target)))
+                       
     return torch.tensor(input_ids, dtype=torch.long), torch.tensor(target, dtype = torch.long)
 
 def train_datasetloader():
@@ -46,7 +50,7 @@ def val_datasetloader():
     return DataLoader(
       dataset = dataset,
       batch_size = BATCH_SIZE,
-      shuffle=True,
+      shuffle=False,
       num_workers=8
     )
 
